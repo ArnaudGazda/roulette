69897cc2f0fd69c235af3912033e22b9809a3831
Arnaud Gazda <arnaud@gazda.fr>
2025-02-21 21:11:04 +0100

    First version of github action

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,10 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  // Title
+  const expected_title = 'Le jeu de la roulette';
+
+  // Before each yesy
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
@@ -17,13 +21,13 @@ describe('AppComponent', () => {
   it(`should have the 'roulette' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('roulette');
+    expect(app.title).toEqual(expected_title);
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, roulette');
+    expect(compiled.querySelector('h1')?.textContent).toContain(expected_title);
   });
 });
